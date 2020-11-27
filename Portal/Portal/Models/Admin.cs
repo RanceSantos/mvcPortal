38b2971b09fd1a0a128b0e2692283ec187dd224b
User <User@DESKTOP-2P68FEM>
2020-11-27 10:09:45 +0000

    Commit a message

---
+++ Portal/Portal/Models/Admin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portal.Models
+{
+    public class Admin
+    {
+        //public class Admin
+       
+         
+            public int ID { get; set; }
+            [DisplayName("Student ID")]
+            public string StudentID { get; set; }
+
+            [DisplayName("Last Name")]
+            public string Last_Name { get; set;}
+            [DisplayName("First Name")]
+            public string First_Name { get; set; }          
+            
+            [DataType(DataType.Date)]
+            public DateTime Birthdate { get; set; }
+            public int Age { get; set; }
+            public string Address { get; set; }
+       
+    }
+}
---
+++ Portal/Portal/Models/SeedData.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Portal.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portal.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new PortalContext(serviceProvider.GetRequiredService<DbContextOptions<PortalContext>>()))
+            {
+                if (context.Admin.Any())
+                {
+                    //if contains any data --- then we return --- we do not seed
+                    return;
+                }
+                context.Admin.AddRange(
+                new Admin
+                {
+                    StudentID = "1079-18",
+                    Last_Name = "Santos",
+                    First_Name = "Lean Andrew",
+                    Birthdate = DateTime.Parse("2020-1-11"),
+                    Age = 21,
+                    Address = "Paranaque City",
+                }
+
+            );
+                context.SaveChanges();
+
+            }
+        }
+    }
+}
